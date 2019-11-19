25115acbf16e4538b4e4fc3d7341d1e98f875432
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-19 23:43:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,15 @@
 class Person 
-  attr_accessor :name, :breed
+  attr_accessor :name
   def initialize(name, breed ="Mutt")
     @name = name
     @breed = breed 
   end
+  
+  def breed=(breed="Mutt")
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed
+  end 
 end 
